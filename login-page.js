3fda4c1d1e1e4ed1e3b91fe0698badb571f9e0b8
ksenia_linguist <kolganova.ksenia@yandex.ru>
2023-03-02 15:41:27 +0400

    new code

--- login-page.js
+++ login-page.js
@@ -1,21 +1,90 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+var form = document.querySelector('#form_id');
+var login = document.querySelector('#email');
+var password = document.querySelector('#password');
 
-// When the login button is clicked, the following code is executed
-loginButton.addEventListener("click", (e) => {
-    // Prevent the default submission of the form
-    e.preventDefault();
-    // Get the values input by the user in the form fields
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+var loginError = document.querySelector('#login-error');
+var passwordError = document.querySelector('#password-error');
 
-    if (username === "morozova@mail.ru" && password === "1234567") {
-        // If the credentials are valid, show an alert box and reload the page
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        // Otherwise, make the login error message show (change its oppacity)
-        loginErrorMsg.style.opacity = 1;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    loginError.innerHTML = '';
+    passwordError.innerHTML = '';
+   
+
+    var loginValue = login.value;
+    var passwordValue = password.value;
+  
+    var loginErr = '';
+    var passwordErr = '';
+    
+
+    if (!(/\S+@[a-z]+.[a-z]+/g.test(loginValue))) {
+        loginErr = 'Введите корректный логин (e-mail)';
     }
-})
+
+    if (!(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/g.test(passwordValue))) {
+        passwordErr = 'Неправильный пароль';
+    }
+
+    if (passwordValue.length < 6) {
+        passwordErr = 'Пароль должен быть длиннее 6 символов';
+    }
+
+
+    loginError.innerHTML = loginErr;
+    passwordError.innerHTML = passwordErr;
+    
+
+    if (loginErr.length === 0 && passwordErr.length === 0 && phoneErr.length === 0) {
+        alert('Успешный вход');
+    }
+});
+
+function clock() {   // дата и время
+    var d = new Date();
+    var month_num = d.getMonth()
+    var day = d.getDate();
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var seconds = d.getSeconds();
+    var weekday_num = d.getDay();
+
+    var weekday= ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    var month = ["января", "февраля", "марта", "апреля", "мая", "июня",
+        "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+
+    if (day <= 9) day = "0" + day;
+    if (hours <= 9) hours = "0" + hours;
+    if (minutes <= 9) minutes = "0" + minutes;
+    if (seconds <= 9) seconds = "0" + seconds;
+
+    date_time = "Сегодня - " + weekday[weekday_num] + ", " + day + " " + month[month_num] + " " + d.getFullYear() +
+        " г.&nbsp;&nbsp;&nbsp;Текущее время - "+ hours + ":" + minutes + ":" + seconds;
+    if (document.layers) {
+        document.layers.doc_time.document.write(date_time);
+        document.layers.doc_time.document.close();
+    } else {
+        document.getElementById("doc_time").innerHTML = date_time;
+    }
+
+    setTimeout("clock()", 1000);
+}
+
+clock();
+
+if (/MSIE 10/i.test(navigator.userAgent)) {
+    // This is internet explorer 10
+    window.alert('isIE10');
+}
+
+if (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+    // This is internet explorer 9 or 11
+    window.location = 'pages/core/ie.htm';
+}
+
+if (/Edge\/\d./i.test(navigator.userAgent)) {
+    // This is Microsoft Edge
+    window.alert('Microsoft Edge');
+}
